8bbf4fcc871f80d3ce718a5515ec4fd57db92f44
Maycon Fabio <m.vinnicyus@gmail.com>
2022-08-20 21:52:07 -0300

    Monta fila de acordo com a semana do ano

--- src/routes/nasa.js
+++ src/routes/nasa.js
@@ -1,46 +1,60 @@
+const dayjs = require("dayjs")
+const weekOfYear = require("dayjs/plugin/weekOfYear")
+dayjs.extend(weekOfYear)
+
 const database = require("../database")
 const webhook = require("../helpers/webhook")
 
 
 class Nasa {
+  constructor(params) {
+    this._params = params
+  }
+
   // Adiciona um membro na nasa
-  async adicionar(params) {
-    const membro = params.join(' ')
+  async adicionar() {
+    const membro = this._params.join(' ')
     database.update("membros-nasa", `${membro}`)
 
     await webhook.send(`Membro <${membro}> adicionado`)
   }
 
   // Puxa o próximo membro da fila
-  async proximo(params) {
+  async proximos() {
     const column = "membros-nasa"
     const membros = database.read(column)
-    const membro = membros[0]
 
-    database.remove(column, membro)
-    database.update(column, membro)
+    const weekNum = dayjs().week()
+    const regerencia = membros[(weekNum + 0) % membros.length]
+    const refofoca = membros[(weekNum + 1) % membros.length]
+    const transparencia = membros[(weekNum + 2) % membros.length]
 
-    await webhook.send(`Próximo membro: <${membro}>`)
+    await webhook.send(`
+      Responsabilidades da semana:
+      \tRegerência: <${regerencia}>
+      \tRefofoca: <${refofoca}>
+      \tTransparência: <${transparencia}>
+    `)
   }
 
   // Retorna a fila de membros sem alterá-la
-  async fila(params) {
+  async fila() {
     const membros = database.read("membros-nasa")
 
     await webhook.send(`Fila:\n${membros.join("\n")}`)
   }
 }
-const nasa = new Nasa()
 
 // função que lida com a requisição vinda do slack
 async function commands(req, res) {
   const [comando, ...params] = req.body.text.split(' ')
+  const nasa = new Nasa(params)
 
   try {
       if (nasa[comando] === undefined) 
         throw new Error(`Comando "${comando}" não encontrado.`)
 
-      await nasa[comando](params)
+      await nasa[comando]()
   } catch(err) {
       await webhook.send(`Deu errado! (${err})`)
   }
